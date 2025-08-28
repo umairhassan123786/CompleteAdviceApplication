0e6bcab1ae13ff1b06f1db2354baeb6e9104750e
MuhammadUmair <umair.infini8ai@gmail.com>
2025-08-28 10:27:23 +0500

    fix: Remove duplicate nested structure and keep only complete src folder

--- AdviceAPIApplication/src/main/java/com/descenedigital/repo/AdviceRepo.java
+++
@@ -1,7 +0,0 @@
-package com.descenedigital.repo;
-
-import com.descenedigital.model.Advice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdviceRepo extends JpaRepository<Advice, Long> {
-}
--- Role.java
+++
@@ -1,5 +0,0 @@
-package com.descenedigital.enums;
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
-}
